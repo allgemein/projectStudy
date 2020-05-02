73b691d7d3c5e0fb8d4e519d63d2fd69c1ef6a2b
allgemein <amanetamada@tamadapennoMacBook-Air.local>
2020-05-02 12:52:55 +0900

    whitenoiseのimportにエラーが出ていたのを直した

--- prototype/serverside/serverside/wsgi.py
+++ prototype/serverside/serverside/wsgi.py
@@ -1,17 +1,10 @@
-"""
-WSGI config for serverside project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
 
-from dj_static import Cling
+#from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'serverside.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myDjango.settings")
 
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
+
+application = get_wsgi_application()
