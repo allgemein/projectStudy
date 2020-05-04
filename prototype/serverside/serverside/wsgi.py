ecaf6cc518a1db66b5f2b113ef83dc875af1a9a7
allgemein <amanetamada@tamadapennoMacBook-Air.local>
2020-05-04 16:28:13 +0900

    gunicornで走らせるための設定を直した

--- prototype/serverside/serverside/wsgi.py
+++ prototype/serverside/serverside/wsgi.py
@@ -3,7 +3,7 @@ import os
 #from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myDjango.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "serverside.settings")
 
 #application = Cling(get_wsgi_application())
 
