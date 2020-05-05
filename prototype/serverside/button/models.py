446c98a74446871d0023d0588fa6f87141bab603
allgemein <amanetamada@tamadapennoMacBook-Air.local>
2020-05-05 16:50:18 +0900

    cssをつけたり，それに伴ってhtmlを書き換えたり

--- prototype/serverside/button/models.py
+++ prototype/serverside/button/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class status(models.Model):
-    place = models.IntegerField(default=0)
+    direction = models.CharField(default="front",max_length=100)
+    power = models.IntegerField(default="0")
     def __str__(self):
-        return str(self.place)
+        return str(self.direction+","+str(self.power))
--- prototype/serverside/button/views.py
+++ prototype/serverside/button/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 from .models import status
@@ -8,21 +8,24 @@ from django.http import HttpResponse
 def index(request):
     template='button/index.html'
     if request.method == 'POST':
-        if 'button_1' in request.POST:
+        tmp=status.objects.last()
+        if(50<status.objects.count()):
             status.objects.all().delete()
-            t=status(place=1)
+            tmp.save()
+        if 'button_1' in request.POST:
+            t=status(direction=("back" if tmp.direction=="front" else "front"),power=1)
             t.save()
         elif 'button_2' in request.POST:
-            status.objects.all().delete()
-            t=status(place=2)
+            t=status(direction=tmp.direction,power=(0 if tmp.power else 1))
             t.save()
-        elif 'button_3' in request.POST:
-            status.objects.all().delete()
-            t=status(place=3)
-            t.save()
-
-    return render(request,template)
+        return redirect("./")
+    context = {
+            'status' : status.objects.last()
+            }
+    return render(request,template,context)
 
 def data(request):
-    res=status.objects.get().place
+    res=(status.objects.last().direction)
+    if(status.objects.last().power):
+        res="stop"
     return HttpResponse(res)
