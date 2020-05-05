11e2460cfb2d0fd74e6640ef6013cce496edbe53
allgemein <amanetamada@tamadapennoMacBook-Air.local>
2020-05-05 16:55:39 +0900

    default値の設定ミスを直した

--- prototype/serverside/button/models.py
+++ prototype/serverside/button/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class status(models.Model):
     direction = models.CharField(default="front",max_length=100)
-    power = models.IntegerField(default="0")
+    power = models.IntegerField(default=0)
     def __str__(self):
         return str(self.direction+","+str(self.power))
