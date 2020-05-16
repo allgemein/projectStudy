2601cdd3def6acabfdf7a545e9651191bba959c5
allgemein <amanetamada@tamadapennoMacBook-Air.local>
2020-05-16 18:04:48 +0900

    static fileを仮置きした

--- src/website/wheelchair/forms.py
+++ src/website/wheelchair/forms.py
@@ -4,4 +4,4 @@ from django.contrib.auth.forms import UserCreationForm
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2',)
